f6b097524bdcdb6a4090ed4ddfdab3c967a9b665
pveggie@hotmail.com <pveggie@hotmail.com>
2018-11-04 19:29:30 +0100

    Automated commit at 2018-11-04 18:29:30 UTC by middleman-deploy 1.0.0

---
+++ javascripts/propaganda.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+
+  $('#propaganda-button').on('click', function(event) {
+    $('#propaganda-dialog').html("<p class='font-large'><strong>I know, right?!</strong></p>");
+  });
+
+
+
+
+});
